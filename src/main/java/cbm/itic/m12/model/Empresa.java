feac045b097008b309e00ac5651c7465ed325a1d
cbernatfp <cbernatfp@gmail.com>
2023-11-12 11:28:32 +0100

    Relació bidireccional sense recursió

--- src/main/java/cbm/itic/m12/model/Empresa.java
+++ src/main/java/cbm/itic/m12/model/Empresa.java
@@ -1,60 +1,98 @@
 package cbm.itic.m12.model;
 
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
+@JsonIdentityInfo(
+		  generator = ObjectIdGenerators.PropertyGenerator.class, 
+		  property = "id_empresa")
 public class Empresa {
 	@Id                  //jakarta.persistence.Id
-	private long id;
+	private long id_empresa;
 
 	@Column(nullable = false, unique = true)
 	private String nom;
 
 	@Column(nullable = false)
 	private String cif;
+	
+    @OneToMany(mappedBy="empresa")
+    @JsonIgnore
+    @JsonManagedReference
+    private Set<Oferta> ofertes;
 
+    
 	// A PARTIR D'AQUÍ CODI GENERAT DES DEL MENÚ "Source": 
 	// Els 2 constructors i els getters i setters
+
 
 	public Empresa() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 
-	public Empresa(long id, String nom, String cif) {
+
+	public Empresa(long id_empresa, String nom, String cif, Set<Oferta> ofertes) {
 		super();
-		this.id = id;
+		this.id_empresa = id_empresa;
 		this.nom = nom;
 		this.cif = cif;
+		this.ofertes = ofertes;
 	}
 
-	public long getId() {
-		return id;
+
+	public long getId_empresa() {
+		return id_empresa;
 	}
 
-	public void setId(long id) {
-		this.id = id;
+
+	public void setId_empresa(long id_empresa) {
+		this.id_empresa = id_empresa;
 	}
+
 
 	public String getNom() {
 		return nom;
 	}
 
+
 	public void setNom(String nom) {
 		this.nom = nom;
 	}
+
 
 	public String getCif() {
 		return cif;
 	}
 
+
 	public void setCif(String cif) {
 		this.cif = cif;
 	}
+
+
+	public Set<Oferta> getOfertes() {
+		return ofertes;
+	}
+
+//
+//	public void setOfertes(Set<Oferta> ofertes) {
+//		this.ofertes = ofertes;
+//	}
+
+	
+	
+
 
 	
 }
--- src/main/java/cbm/itic/m12/model/Oferta.java
+++ src/main/java/cbm/itic/m12/model/Oferta.java
@@ -1,14 +1,33 @@
 package cbm.itic.m12.model;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 
 @Entity
+@JsonIdentityInfo(
+		  generator = ObjectIdGenerators.PropertyGenerator.class, 
+		  property = "id_oferta")
 public class Oferta {
 	@Id                  //jakarta.persistence.Id
-	private long id;
-
+	private long id_oferta;
+	
+	@ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "id_empresa", nullable = false)
+	@OnDelete(action = OnDeleteAction.CASCADE)
+//  @JsonIgnore
+//	@JsonBackReference
+    private Empresa empresa;	
+	
 	@Column(nullable = false, unique = true)
 	private String lloc;
 
@@ -20,19 +39,28 @@ public class Oferta {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Oferta(long id, String lloc, String jornada) {
+	public Oferta(long id_oferta, Empresa empresa, String lloc, String jornada) {
 		super();
-		this.id = id;
+		this.id_oferta = id_oferta;
+		this.empresa = empresa;
 		this.lloc = lloc;
 		this.jornada = jornada;
 	}
 
-	public long getId() {
-		return id;
+	public long getId_oferta() {
+		return id_oferta;
 	}
 
-	public void setId(long id) {
-		this.id = id;
+	public void setId_oferta(long id_oferta) {
+		this.id_oferta = id_oferta;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
 	}
 
 	public String getLloc() {
@@ -50,14 +78,11 @@ public class Oferta {
 	public void setJornada(String jornada) {
 		this.jornada = jornada;
 	}
-
-	@Override
-	public String toString() {
-		return "Oferta [id=" + id + ", lloc=" + lloc + ", jornada=" + jornada + "]";
-	}
 	
 	
 
+	
+	
 }
 
 
